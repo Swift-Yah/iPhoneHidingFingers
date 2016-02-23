7c8209aecb2cc4017854cd909eda543d1e5d81eb
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-02-23 20:07:53 -0300

    Basic project structure.

---
+++ iPhoneHidingFingers/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  iPhoneHidingFingers
+//
+//  Created by Rafael Ferreira on 2/23/16.
+//  Copyright © 2016 Data Empire. All rights reserved.
+//
+
+import UIKit
+
+/** The view controller for the home view at `Main`.storyboard. */
+public class HomeViewController: UIViewController {
+    
+}
+
