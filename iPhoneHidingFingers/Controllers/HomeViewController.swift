31999d29f2cf648ee2ce671b0051807fc14c22f6
Rafael da Silva Ferreira <rafael_yami@hotmail.com>
2016-02-23 21:08:13 -0300

    Added the UI and basic function to change the UI.
    The UI respond the answer of the user.
    If the user match the answer the UI will show it, and the same occurs when the user make a mistake.

--- iPhoneHidingFingers/Controllers/HomeViewController.swift
+++ iPhoneHidingFingers/Controllers/HomeViewController.swift
@@ -11,5 +11,62 @@ import UIKit
 /** The view controller for the home view at `Main`.storyboard. */
 public class HomeViewController: UIViewController {
     
+    @IBOutlet weak var statusImageView: UIImageView!
+    @IBOutlet weak var attemptTextField: UITextField!
+    
+    private var numberToHide: Int?
+    
+    // MARK: UIView methods
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        generateHideNumber()
+    }
+    
+    // MARK: Helper methods
+    
+    private func generateHideNumber() {
+        let randomDouble: Double = drand48()
+        let random: Double = randomDouble * 10
+        
+        numberToHide = Int(random)
+        print(numberToHide!)
+    }
+    
+    private func checkCorrectAnswer() -> Bool {
+        if let typedNumber = attemptTextField.text {
+            let number: Int = Int(typedNumber)!
+            let isCorrect: Bool = number == numberToHide
+            
+            if isCorrect {
+                generateHideNumber()
+            }
+            
+            return isCorrect
+        }
+        
+        return false
+    }
+    
+    // MARK: Action methods
+    
+    @IBAction func tryFindOutAction() {
+        let isCorrect: Bool = checkCorrectAnswer()
+        let imageName = isCorrect ? "Awesome" : "Failure"
+        
+        UIView.animateWithDuration(1.5, animations: { () -> Void in
+            self.statusImageView.image = UIImage(imageLiteral: imageName)
+            self.view.backgroundColor = isCorrect ? UIColor.greenColor() : UIColor.redColor()
+            }) { (finished: Bool) -> Void in
+                if (finished) {
+                    self.statusImageView.image = UIImage(imageLiteral: "Charade")
+                    self.attemptTextField.text = String()
+                    self.view.backgroundColor = UIColor.blueColor()
+                }
+                
+        }
+    }
+    
 }
 
